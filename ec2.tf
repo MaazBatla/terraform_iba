20c7c0de246acc4d84d9789da396ab6576b35b32
Muhammad Maaz Arsalan Batla <m.batla.22794@khi.iba.edu.pk>
2025-04-27 02:23:00 +0500

    Terraform Setup Complete
    Initial Terraform setup complete without user files.

--- ec2.tf
+++ ec2.tf
@@ -1,10 +1,10 @@
 # ec2.tf
 
 resource "aws_instance" "ubuntu_instance" {
-  ami                    = "ami-08e4b984abde34a4f" # Ubuntu 20.04 AMI ID
+  ami                    = "ami-084568db4383264d4" # Ubuntu 20.04 AMI ID
   instance_type          = "t2.micro"
-  subnet_id              = "subnet-a1a969d6" # Hardcoded subnet ID for Ubuntu instance
-  key_name               = "tf_test"
+  subnet_id              = "subnet-0a1d914795c0fd422" # Hardcoded subnet ID for Ubuntu instance
+  key_name               = "maaz-aws-server-key"
   security_groups        = [aws_security_group.ubuntu_sg.id] # Assign Ubuntu security group
   tags = {
     Name = "tf_created_ec2_ubuntu"
@@ -14,15 +14,13 @@ resource "aws_instance" "ubuntu_instance" {
     volume_size = 8
     volume_type = "gp3"
   }
-
-  user_data = templatefile("${path.module}/ec2_userdata/ubuntu_userdata.sh", {})
 }
 
 resource "aws_instance" "amazon_linux_2_instance" {
-  ami                    = "ami-04f73ca9a4310089f" # Amazon Linux 2 AMI ID
+  ami                    = "ami-0b86aaed8ef90e45f" # Amazon Linux 2 AMI ID
   instance_type          = "t2.micro"
-  subnet_id              = "subnet-3e5c0878" # Hardcoded subnet ID for Amazon Linux 2 instance
-  key_name               = "tf_test"
+  subnet_id              = "subnet-0de41697a8bf030a3" # Hardcoded subnet ID for Amazon Linux 2 instance
+  key_name               = "maaz-aws-server-key"
   security_groups        = [aws_security_group.amazon_linux_sg.id] # Assign Amazon Linux 2 security group
   tags = {
     Name = "tf_created_ec2_amazon_linux_2"
@@ -32,15 +30,13 @@ resource "aws_instance" "amazon_linux_2_instance" {
     volume_size = 8
     volume_type = "gp3"
   }
-
-  user_data = templatefile("${path.module}/ec2_userdata/al2_userdata.sh", {})
 }
 
 resource "aws_instance" "amazon_linux_2023_instance" {
-  ami                    = "ami-0dc44e17251b74325" # Amazon Linux 2023 AMI ID
+  ami                    = "ami-0e449927258d45bc4" # Amazon Linux 2023 AMI ID
   instance_type          = "t2.micro"
-  subnet_id              = "subnet-c742efa2" # Hardcoded subnet ID for Amazon Linux 2023 instance
-  key_name               = "tf_test"
+  subnet_id              = "subnet-04390ee5cbac457cd" # Hardcoded subnet ID for Amazon Linux 2023 instance
+  key_name               = "maaz-aws-server-key"
   security_groups        = [aws_security_group.amazon_linux_sg.id] # Assign Amazon Linux 2023 security group
   tags = {
     Name = "tf_created_ec2_amazon_linux_2023"
@@ -50,6 +46,4 @@ resource "aws_instance" "amazon_linux_2023_instance" {
     volume_size = 8
     volume_type = "gp3"
   }
-
-  user_data = templatefile("${path.module}/ec2_userdata/al2023_userdata.sh", {})
 }
